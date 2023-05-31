12cc798e35a20a469365a1695025689e46792aae
Alvaro Dwi <varoa.dwi@gmail.com>
2023-05-31 15:51:24 +0700

    add maze config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,4 +28,31 @@ export default defineNuxtConfig({
 	build: {
 		transpile: ['@vuepic/vue-datepicker', '@headlessui/vue'],
 	},
+	app: {
+		head: {
+			script: [
+				{
+					textContent: `(function (m, a, z, e) {
+						var s, t;
+						try {
+						  t = m.sessionStorage.getItem('maze-us');
+						} catch (err) {}
+					  
+						if (!t) {
+						  t = new Date().getTime();
+						  try {
+							m.sessionStorage.setItem('maze-us', t);
+						  } catch (err) {}
+						}
+					  
+						s = a.createElement('script');
+						s.src = z + '?t=' + t + '&apiKey=' + e;
+						s.async = true;
+						a.getElementsByTagName('head')[0].appendChild(s);
+						m.mazeUniversalSnippetApiKey = e;
+					  })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '297d9c3d-1eee-4cc4-b6c1-4d406fc8f3fe');`,
+				},
+			],
+		},
+	},
 })
